ee94e24cb9a173077a0f4feaa9000687619bb6bd
Thomas R <contact@lgmcreation.fr>
2019-05-10 09:38:14 +0200

    Update security.php

--- lib/security.php
+++ lib/security.php
@@ -4,15 +4,12 @@ if ( !defined('ABSPATH') ) die();
 * SECURITY
 **/
 
+
 	/*Masque les erreurs de connexions*/
-<<<<<<< HEAD
 	function lgm_no_wordpress_errors(){
-		return 'Erreur de connexion!';
+		return 'Error!';
 	}
 	add_filter( 'login_errors', 'lgm_no_wordpress_errors' );
-=======
-	add_filter('login_errors',create_function('$a', "return 'Erreur';"));
->>>>>>> 679d478cd8fd68f5df856e57fca5046629aec3b9
 
 	/*Disable XML RPC*/
 	add_filter( 'xmlrpc_enabled', '__return_false' );
